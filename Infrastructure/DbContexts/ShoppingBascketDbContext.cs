71e8885143f6903ff6ca2039e9d41637a632760d
farha <farha@FARHANG>
2024-05-07 22:23:00 +0330

    Add project files.

---
+++ Infrastructure/DbContexts/ShoppingBascketDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Services.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DbContexts
+{
+    internal class ShoppingBascketDbContext : DbContext
+    {
+        public DbSet<Person> MyProperty { get; set; }
+
+    }
+}
---
+++ Infrastructure/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using Infrastructure.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public static class StartUp
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddDbContext<ShoppingBascketDbContext>(options => options.UseSqlServer(""));
+        }
+    }
+}
